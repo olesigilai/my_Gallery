0ce4380bb0abb092bab5f39f19db67048b429f84
olesigilai <ole_sigilai@yahoo.com>
2021-03-20 17:56:16 +0300

    imported datetime and creating the first class Location and assigning a character field

--- images/models.py
+++ images/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+import cloudinary
+from cloudinary.models import CloudinaryField
+import datetime as dt
+
 
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length = 55)
+    
+
