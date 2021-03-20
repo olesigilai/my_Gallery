884b53a627b1e2542bcd40396b076e82f67e93a5
olesigilai <ole_sigilai@yahoo.com>
2021-03-20 15:22:29 +0300

    impoRted HttpResponse ,image and define gallery function that is returned at gallery html

--- images/views.py
+++ images/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Image,Location,Category
+from django.http import HttpResponse, Http404
+import datetime as dt
+
 
 # Create your views here.
+def my_gallery(request):
+    date = dt.date.today()
+    images = Image.objects.all()
+    return render(request, 'gallery.html', {"date": date,"images":images})
