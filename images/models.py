a53a4647dd8074025ca2828ae7c850e865f066eb
olesigilai <ole_sigilai@yahoo.com>
2021-03-20 20:58:14 +0300

    classmethod for update the location is added

--- images/models.py
+++ images/models.py
@@ -7,5 +7,10 @@ import datetime as dt
 # Create your models here.
 class Location(models.Model):
     name = models.CharField(max_length = 55)
-    
+
+    @classmethod
+    def update_location(cls,id,value):
+        cls.objects.filter(id=id).update(name = value)
+    def __str__(self):
+        return self.name
 
