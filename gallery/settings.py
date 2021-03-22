8ba81ba5cb10ed80a799ea8fe3853fd6d7823287
olesigilai <ole_sigilai@yahoo.com>
2021-03-22 12:05:17 +0300

    htmls are changed so as to use asingle javascript function for copy

--- gallery/settings.py
+++ gallery/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import cloudinary
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+os.path.join(BASE_DIR,"static"),
+]
 
 cloudinary.config( 
   cloud_name = "olesigilai", 
--- gallery/urls.py
+++ gallery/urls.py
@@ -18,6 +18,5 @@ from django.urls import include,path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('images/',include('images.urls')),
     path('',include('images.urls'))
 ]
